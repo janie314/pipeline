a3bc72a231ea1701a4c2d8e4fa6c54ce041303c0
12ball <pub@janie.page>
2025-02-23 08:15:55 -0600

    1740320155

--- Rakefile
+++ Rakefile
@@ -2,9 +2,15 @@ require "peppermint/rake"
 require_relative "lib/imports/networkworld"
 require_relative "lib/consts"
 
+desc "Installs dependencies"
+task :deps do
+  sh "pip install --upgrade csvtotable"
+end
+
 desc "Starts the pipeline"
 task :start do
-  NetworkWorld.new.run
+  SemanticLogger.add_appender(io: $stdout, formatter: :color)
+  NetworkWorld.run
 end
 
 desc "Prints current version"
--- lib/import.rb
+++ lib/import.rb
@@ -1,4 +1,30 @@
-def import_to_json(name, data)
-  FileUtils.mkdir_p(File.join(__dir__, "../../data/imports"))
-  File.write(File.join(__dir__, "../../data/imports", "#{name}.json"), data.to_json)
+require "semantic_logger"
+require "csv"
+
+class Import
+  def self.log = SemanticLogger["import"]
+
+  def self.json(name, data)
+    FileUtils.mkdir_p(File.join(__dir__, "../../data/imports"))
+    File.write(File.join(__dir__, "../../data/imports", "#{name}.json"), data.to_json)
+  end
+
+  def self.csv(name, data)
+    log.info "importing #{name} to csv"
+    FileUtils.mkdir_p(File.join(__dir__, "../../data/imports"))
+    csvpath = File.join(__dir__, "../data/imports", "#{name}.csv")
+    CSV.open(csvpath, "w") do |csv|
+      headers = data.first&.keys
+      if headers.nil?
+        log.warn "no data from #{name} query"
+      else
+        csv << headers
+        data[1..].each do |row|
+          csv << headers.map { |h| row[h] }
+        end
+      end
+    end
+    htmlpath = File.join(__dir__, "../data/imports", "#{name}.html")
+    system "csvtotable", csvpath, htmlpath
+  end
 end
