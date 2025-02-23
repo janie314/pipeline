a74252b9e0e46446cd30bb8f39444a35bee1c8f7
Jane Davis <61095764+janie314@users.noreply.github.com>
2025-02-23 08:16:14 -0600

    v0.1.3 (#7)
    Co-authored-by: 12ball <pub@janie.page>

--- Gemfile
+++ Gemfile
@@ -7,3 +7,7 @@ gem "peppermint", "~> 0.1.19", group: :development
 gem "http", "~> 5.2"
 
 gem "nokogiri", "~> 1.18"
+
+gem "csv", "~> 3.3"
+
+gem "semantic_logger", "~> 4.16"
--- lib/imports/networkworld.rb
+++ lib/imports/networkworld.rb
@@ -4,7 +4,10 @@ require "json"
 require_relative "../import"
 
 class NetworkWorld
-  def run
+  def self.log = SemanticLogger["NetworkWorld"]
+
+  def self.run
+    log.info "running NetworkWorld query"
     res = `curl -s 'https://nitter.net/search?f=tweets&q=networkworld' --compressed -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:135.0) Gecko/20100101 Firefox/135.0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept-Encoding: gzip, deflate, br, zstd' -H 'Connection: keep-alive' -H 'Upgrade-Insecure-Requests: 1' -H 'Sec-Fetch-Dest: document' -H 'Sec-Fetch-Mode: navigate' -H 'Sec-Fetch-Site: none' -H 'Sec-Fetch-User: ?1' -H 'Priority: u=0, i' -H 'TE: trailers'`
     html = Nokogiri::HTML.parse(res)
     data = []
@@ -14,6 +17,6 @@ class NetworkWorld
         tweet: tweet.text
       })
     end
-    import_to_json("networkworld", data)
+    Import.csv("networkworld", data)
   end
 end
