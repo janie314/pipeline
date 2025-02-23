fcd55ddb6364c6ea886b624c8c1bb739968c8452
12ball <pub@janie.page>
2025-02-22 23:14:54 -0600

    fix

--- lib/import.rb
+++ lib/import.rb
@@ -1,4 +1,4 @@
-def import_to_json(name)
+def import_to_json(name, data)
   FileUtils.mkdir_p(File.join(__dir__, "../../data/imports"))
   File.write(File.join(__dir__, "../../data/imports", "#{name}.json"), data.to_json)
 end
