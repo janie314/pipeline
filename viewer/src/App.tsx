3c8bcae86176f0a04dbfaea3995720066c8e87e5
12ball <pub@janie.page>
2025-04-20 20:06:15 -0500

    Setup data viewer

---
+++ viewer/src/App.tsx
@@ -0,0 +1,29 @@
+import "./App.css";
+import DataTable from "datatables.net-react";
+import DT from "datatables.net-dt";
+import { useState } from "react";
+
+DataTable.use(DT);
+
+function App() {
+	const [tableData] = useState([
+		["Tiger Nixon", "System Architect"],
+		["Garrett Winters", "Accountant"],
+	]);
+	return (
+		<>
+			<div>
+				<DataTable data={tableData} className="display">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Position</th>
+						</tr>
+					</thead>
+				</DataTable>
+			</div>
+		</>
+	);
+}
+
+export default App;
