6f25a3d5278d22e479cdae019bd947219b06fd42
12ball <pub@janie.page>
2025-04-20 20:18:15 -0500

    1745198295

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -6,24 +6,24 @@ import { useState } from "react";
 DataTable.use(DT);
 
 function App() {
-	const [tableData] = useState([
-		["Tiger Nixon", "System Architect"],
-		["Garrett Winters", "Accountant"],
-	]);
-	return (
-		<>
-			<div>
-				<DataTable data={tableData} className="display">
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Position</th>
-						</tr>
-					</thead>
-				</DataTable>
-			</div>
-		</>
-	);
+  const [tableData] = useState([
+    ["Tiger Nixon", "System Architect"],
+    ["Garrett Winters", "Accountant"],
+  ]);
+  return (
+    <>
+      <div>
+        <DataTable data={tableData} className="display">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Position</th>
+            </tr>
+          </thead>
+        </DataTable>
+      </div>
+    </>
+  );
 }
 
 export default App;
